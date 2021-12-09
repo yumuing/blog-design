7940590f623bc714e20edc4b1d01b83bb9861838
yuzhe <yuzhe780@126.com>
2021-12-09 22:34:19 +0800

    制作完导航栏基本样式

---
+++ js/plug.js
@@ -0,0 +1,7 @@
+<script type="text/javascript" src="http://cdn.repository.webfont.com/wwwroot/js/wf/youziku.api.min.js"></script>
+<script type="text/javascript">
+   $webfont.load("body", "bdc522e3810e44fb819d8624c0f58c36", "cyjianxk");
+   /*$webfont.load("#id1,.class1,h1", "bdc522e3810e44fb819d8624c0f58c36", "cyjianxk");*/
+   /*．．．*/
+   $webfont.draw();
+</script>
