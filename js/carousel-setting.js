5e62c1c45df17b290534cc530cd32ce0e47566e3
yuzhe <yuzhe780@126.com>
2021-12-13 20:25:34 +0800

    完成轮播图

---
+++ js/carousel-setting.js
@@ -0,0 +1,31 @@
+new SimSwiper("#swiper-demo", {
+	autoplay: 3000,
+	duration: 500,
+	easing: 'ease',
+	button: {
+		prev: "#prev", // 前进后退按钮
+		next: "#next"
+	},
+	pagination: {
+		el: '.pagination',
+		click: true // 是否可以点击
+	},
+	// 轮播图数据
+	data: [{
+		index: 0,
+		href: '#',
+		src: 'img/carousel-1.jpg'
+	}, {
+		index: 1,
+		href: '#',
+		src: 'img/carousel-2.png'
+	}, {
+		index: 2,
+		href: '#',
+		src: 'img/carousel-3.jpg'
+	}, {
+		index: 3,
+		href: '#',
+		src: 'img/carousel-4.jpg'
+	}]
+});
