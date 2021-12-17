7518933fe3b3fbccba8b38dc1fd96056e4a28e66
yuzhe <yuzhe780@126.com>
2021-12-17 13:52:02 +0800

    修复首次进入回到顶部底部按钮显示问题

--- js/toTop-Bottom.js
+++ js/toTop-Bottom.js
@@ -1,13 +1,9 @@
 // 设置回到顶部底部
 $(document).ready(function(){
-	var isTopClick = true;
-	$('#toTop').bind("click", function () {
-		if (isTopClick) {
-			isTopClick = false;
-			$("html,body").animate({scrollTop:0},800);
-		}
-		setTimeout(function () {isTopClick = true;}, 1000);
-	});
+	if($(document).scrollTop() < 300){
+		$("#toBottom").show();
+		$("#toTop").hide();
+	}
 	var isBottomClick = true;
 	$('#toBottom').bind("click", function () {
 		if (isBottomClick) {
@@ -16,11 +12,19 @@ $(document).ready(function(){
 		}
 		setTimeout(function () {isBottomClick = true;},1000);
 	});
+	var isTopClick = true;
+	$('#toTop').bind("click", function () {
+		if (isTopClick) {
+			isTopClick = false;
+			$("html,body").animate({scrollTop:0},800);
+		}
+		setTimeout(function () {isTopClick = true;}, 1000);
+	});
 	$(window).scroll(function(){
 		var topp = $(document).scrollTop();
 		if(topp < 300){
-			$("#toTop").hide();
 			$("#toBottom").show();
+			$("#toTop").hide();
 		}
 		else{
 			$("#toTop").show();
